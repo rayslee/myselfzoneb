c54fdf0830a04ca80e53347ed58d69f985220189
Raymond Lee <15537887071@163.com>
2022-11-28 21:19:04 +0800

    enpower scss with global variables

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Scss from './components/Scss.vue'
 </script>
 
 <template>
@@ -14,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <Scss />
 </template>
 
 <style scoped>
---
+++ src/components/Scss.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.test {
+    width: 100px;
+    height: 100px;
+    background-color: $bgcolor;
+}
+</style>
